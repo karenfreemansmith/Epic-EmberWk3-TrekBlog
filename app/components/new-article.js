c4c0196facba6b62998e3a6039a7ee9adc0b7b00
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2016-10-27 06:47:54 -0700

    display add article form

--- app/components/new-article.js
+++ app/components/new-article.js
@@ -1,4 +1,22 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  addNewSrticle: false,
+  actions: {
+    articleFormShow() {
+      this.set('addNewArticle', true);
+    },
+    saveArticle() {
+      var params = {
+        author: this.get('author') ? this.get('author') : "",
+        headline: this.get('headline') ? this.get('headline') : "",
+        content: this.get('content') ? this.get('content') : "",
+        category: this.get('category' ? this.get('category') : 8),
+        image: this.get('image') ? this.get('image') : "",
+        stardate: this.get('stardate') ? this.get('stardate') : "",
+      };
+      this.set('addNewArticle', false);
+      this.sendAction('createArticle', params);
+    }
+  }
 });
--- app/routes/admin.js
+++ app/routes/admin.js
@@ -4,4 +4,16 @@ export default Ember.Route.extend({
   model() {
     return this.store.findAll('article');
   },
+
+  actions: {
+    createArticle(params) {
+      var newArticle = this.store.createRecord('article', params);
+      newArticle.save();
+      this.transitionTo('admin');
+    },
+    destroyArticle(article) {
+      article.destroyRecord();
+      this.transitionTo('admin');
+    }
+  }
 });
